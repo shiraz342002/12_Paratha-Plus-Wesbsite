9e59e8fba69e708ad2e195ac8ebb26292c43fa5c
Shiraz Mazhar <imshiraz007@gmail.com>
2024-06-20 18:14:19 +0500

    Added Recipe Page

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//For implementing Responsive buttons
 const nav_menu=document.querySelector('.nav_menu');
 const nav_toggle=document.querySelector('.toggle_nav');
 const nav_close=document.querySelector('.close_nav');
@@ -11,4 +12,22 @@ if(nav_close){
         nav_menu.classList.remove('show_menu')
     })
 }
+//To show responsive navbar
+const nav_link=document.querySelectorAll('.nav_links');
+const action_link=()=>{
+    const nav_menu=document.querySelector('.nav_menu')
+    nav_menu.classList.remove('show_menu');
+}
+nav_link.forEach(n=> n.addEventListener('click',action_link))
 
+
+//To show a boxshadow if scroll below header
+const shadowHeader=()=>{
+    const header = document.querySelector('.header')
+    if(window.scrollY>=50){
+        header.classList.add('shadow_header');
+    }else{
+        header.classList.remove('shadow_header');
+    }
+}
+window.addEventListener('scroll',shadowHeader)
